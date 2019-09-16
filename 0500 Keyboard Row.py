eda887278e6bceadacf62bf39ff8636ecb6614ae
littleBlack <2428350606@qq.com>
2019-09-16 14:25:28 +0800

    Create 0500 Keyboard Row.py

---
+++ 0500 Keyboard Row.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        set1 = set('qwertyuiop')
+        set2 = set('asdfghjkl')
+        set3 = set('zxcvbnm')
+        res = []
+        for i in words:
+            x = i.lower()
+            setx = set(x)
+            if setx<=set1 or setx<=set2 or setx<=set3:
+                res.append(i)
+        return res
